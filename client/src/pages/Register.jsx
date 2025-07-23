47acda50e5821de967a8f6fc78768ea1ff6b159f
Angela Gao <ag8969@nyu.edu>
2025-07-23 13:31:40 -0400

    Build register layout with left and right panel

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import logo from "../assets/black-logo.png";
+import logo from "../assets/socotec_img.png";
 import logo_with_title from "../assets/socotec_img.png";
 import {useDispatch, useSelector} from "react-redux"
 import {useNavigate} from "react-router-dom"
@@ -50,15 +50,35 @@ const Register = () => {
   <div className="flex flex-col justify-center md:flex-row h-screen">
     {/*Left Side*/}
     <div className="hidden w-full md:w-1/2 bg-gray-200 text-black md:flex flex-col items-center justify-center p-8 rounded-tr-[80px] rounded-br-[80px]">
-      <div>
-        <div>
-          <img src ={logo_with_title} alt="logo"/>
+      <div className="text-center h-[376px]">
+        <div className="flex justify-center mb-12">
+          <img src ={logo_with_title} alt="logo" className="mb-12 h-44 w-auto"/>
         </div>
-        <p>Already Have Account? Sign in now.</p>
-        <Link to={"/login"}>SIGN IN</Link>
+        <p className="text-black-300 mb-12">Already Have Account? Sign in now.</p>
+        <Link to={"/login"} className="border-2 rounded-lg font-semibold border-black py-2 px-8 hover:bg-black hover:text-white transition">SIGN IN</Link>
+        
       </div>
     </div>
     {/*Right Side */}
+    <div className="w-full md:w-1/2 flex items-center justify-center bg-white p-8">
+      <div className="w-full max-w-sm">
+        <div className="flex justify-center mb-12">
+          <div className="flex flex-col-reverse sm:flex-row items-center justify-center gap-5">
+            <h3 className="font-medium text-4xl overflow-hidden">Sign Up</h3>
+            <img src={logo} alt="logo" className="h-auto w-24 object-cover"/>
+          </div>
+        </div>
+
+
+
+          <p></p>
+
+
+
+
+          
+      </div>
+    </div>
 
   </div>
   
